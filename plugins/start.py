c3c385162721699e4bdb6e5625ad4057ab116e39
devamachra <137611449+devamachra@users.noreply.github.com>
2023-06-24 15:46:07 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,25 @@
+from .. import bot
+from telethon import events
+import asyncio
+
+
+
+@bot.on(events.NewMessage(incoming=True,pattern="/start"))
+async def start(event):
+  await event.reply("Hello This is Inffiix_Bot")
+  
+  
+@bot.on(events.NewMessage(incoming=True , pattern="/get"))
+async def start(event):
+  await event.reply("Hello This is get command")
+async def start(event):
+  a = await event.reply("Hello! This is get command")
+  await asyncio.sleep(1)
+  await a.edit("This is edited message")
+  
+@bot.on(events.NewMessage(incoming=True, pattern="/eval"))
+async def start(event):
+  await event.reply("Hello! This is eval command")
+@bot.on(events.NewMessage(incoming=True, pattern="/hello"))
+async def start(event):
+  await event.reply("Hello this is hello command")
